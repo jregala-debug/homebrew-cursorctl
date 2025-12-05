a6b1644073515d7562a7acb44464f833b88c9306
jregala-debug <jerichomangalindan@ymail.com>
2025-12-05 12:51:56 -0500

    Update cursorctl.rb

--- Formula/cursorctl.rb
+++ Formula/cursorctl.rb
@@ -1,7 +1,7 @@
 class Cursorctl < Formula
   desc "Command-line tool to hide/show the macOS cursor instantly"
   homepage "https://github.com/Jregala-debug/homebrew-cursorctl"
-  url "https://github.com/Jregala-debug/homebrew-cursorctl/releases/download/v1.0.0/cursorctl-macos.zip"
+  url "https://github.com/jregala-debug/homebrew-cursorctl/releases/download/v1.0.0/cursorctl-macos.zip"
   sha256 "d28dadfee3aa1d4ead7c0133b9294cf8fff857f09a3bfc998309118cf1069b1f"
   version "1.0.0"
 
